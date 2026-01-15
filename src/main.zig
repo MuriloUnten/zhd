92b1952d2b6a7e154a1e248069213552fa6d096c
Murilo Unten <murilo.unten@proton.me>
2026-01-15 16:04:28 -0300

    feat: add second space after 8th byte && rename some variables

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,6 @@ const fs = std.fs;
 // TODO
 // add optional cmd line flags
 // add multithread support
-// add second space after 8th byte in the line
 // check if adjacent output lines are identical and omit replicas
 
 pub fn main() !void {
@@ -13,9 +12,9 @@ pub fn main() !void {
     // var file: fs.File = try fs.openSelfExe(.{});
     defer file.close();
 
-    var buf = std.mem.zeroes([4]u8);
-    var buf2 = std.mem.zeroes([4]u8);
-    var reader = file.reader(&buf);
+    var reader_buffer = std.mem.zeroes([6]u8);
+    var line_buffer = std.mem.zeroes([4]u8);
+    var reader = file.reader(&reader_buffer);
 
     var line_byte_count: u32 = 0;
     var bytes_in_line: u8 = 0;
@@ -23,16 +22,16 @@ pub fn main() !void {
 
     // TODO refactor code to print a whole line all at once
     while (true) {
-        const n = try reader.interface.readSliceShort(&buf2);
+        const n = try reader.interface.readSliceShort(&line_buffer);
         var i: usize = 0;
         while (i < n) : (i += 1) {
             if (bytes_in_line == 0) {
                 std.debug.print("{X:0>7} ", .{line_byte_count});
             }
-            std.debug.print("{X:0>2}", .{buf[i]});
+            std.debug.print("{X:0>2}", .{line_buffer[i]});
             // // print byte as ascii if it's a regular printable character
-            // if (buf2[i] >= '!' and buf2[i] <= '~') {
-            //     std.debug.print("{c}", .{buf2[i]});
+            // if (line_buffer[i] >= '!' and line_buffer[i] <= '~') {
+            //     std.debug.print("{c}", .{line_buffer[i]});
             // }
             // else {
             //     std.debug.print(".", .{});
@@ -43,6 +42,9 @@ pub fn main() !void {
 
             if (bytes_in_sequence == 2) {
                 bytes_in_sequence = 0;
+                std.debug.print(" ", .{});
+            }
+            if (bytes_in_line == 8) {
                 std.debug.print(" ", .{});
             }
             if (bytes_in_line == 16) {
